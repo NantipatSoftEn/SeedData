4025d5349a13ebb28ae1a210ea2912c3b2bf77c2
NantipatSoftEn <keymasterviriya1150@gmail.com>
2026-01-21 02:38:45 +0700

    chore: add user image
    - Added new user image file

--- src/components/user-profile/UserInfoCard.tsx
+++ src/components/user-profile/UserInfoCard.tsx
@@ -14,56 +14,80 @@ export default function UserInfoCard() {
     closeModal();
   };
   return (
-    <div className="p-5 border border-gray-200 rounded-2xl dark:border-gray-800 lg:p-6">
+    <div className="rounded-2xl border border-gray-200 p-5 lg:p-6 dark:border-gray-800">
       <div className="flex flex-col gap-6 lg:flex-row lg:items-start lg:justify-between">
         <div>
-          <h4 className="text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-6">
+          <h4 className="text-lg font-semibold text-gray-800 lg:mb-6 dark:text-white/90">
             Personal Information
           </h4>
 
           <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-7 2xl:gap-x-32">
             <div>
               <p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
-                First Name
+                NickName
               </p>
               <p className="text-sm font-medium text-gray-800 dark:text-white/90">
-                Musharof
+                Bonus
               </p>
             </div>
 
             <div>
               <p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
-                Last Name
+                Gender
               </p>
               <p className="text-sm font-medium text-gray-800 dark:text-white/90">
-                Chowdhury
+                Female
               </p>
             </div>
 
             <div>
               <p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
-                Email address
+                Age & Birthday
               </p>
               <p className="text-sm font-medium text-gray-800 dark:text-white/90">
-                randomuser@pimjo.com
+                28 (April 15, 1995)
               </p>
             </div>
 
             <div>
               <p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
-                Phone
+                Blood Group
               </p>
               <p className="text-sm font-medium text-gray-800 dark:text-white/90">
-                +09 363 398 46
+                O+
               </p>
             </div>
 
             <div>
               <p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
-                Bio
+                Personality
               </p>
               <p className="text-sm font-medium text-gray-800 dark:text-white/90">
-                Team Manager
+                ENFJ
+              </p>
+            </div>
+            <div>
+              <p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
+                Hobby
+              </p>
+              <p className="text-sm font-medium text-gray-800 dark:text-white/90">
+                Reading,Sleep
+              </p>
+            </div>
+            <div>
+              <p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
+                อาหารที่ชอบ
+              </p>
+              <p className="text-sm font-medium text-gray-800 dark:text-white/90">
+                ก๋วยเตี๋ยวเรือ
+              </p>
+            </div>
+            <div>
+              <p className="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
+                อาหารที่ไม่ชอบ
+              </p>
+              <p className="text-sm font-medium text-gray-800 dark:text-white/90">
+                หัวหอม
               </p>
             </div>
           </div>
@@ -71,7 +95,7 @@ export default function UserInfoCard() {
 
         <button
           onClick={openModal}
-          className="flex w-full items-center justify-center gap-2 rounded-full border border-gray-300 bg-white px-4 py-3 text-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200 lg:inline-flex lg:w-auto"
+          className="shadow-theme-xs flex w-full items-center justify-center gap-2 rounded-full border border-gray-300 bg-white px-4 py-3 text-sm font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-800 lg:inline-flex lg:w-auto dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"
         >
           <svg
             className="fill-current"
@@ -92,20 +116,20 @@ export default function UserInfoCard() {
         </button>
       </div>
 
-      <Modal isOpen={isOpen} onClose={closeModal} className="max-w-[700px] m-4">
-        <div className="no-scrollbar relative w-full max-w-[700px] overflow-y-auto rounded-3xl bg-white p-4 dark:bg-gray-900 lg:p-11">
+      <Modal isOpen={isOpen} onClose={closeModal} className="m-4 max-w-[700px]">
+        <div className="no-scrollbar relative w-full max-w-[700px] overflow-y-auto rounded-3xl bg-white p-4 lg:p-11 dark:bg-gray-900">
           <div className="px-2 pr-14">
             <h4 className="mb-2 text-2xl font-semibold text-gray-800 dark:text-white/90">
               Edit Personal Information
             </h4>
-            <p className="mb-6 text-sm text-gray-500 dark:text-gray-400 lg:mb-7">
+            <p className="mb-6 text-sm text-gray-500 lg:mb-7 dark:text-gray-400">
               Update your details to keep your profile up-to-date.
             </p>
           </div>
           <form className="flex flex-col">
             <div className="custom-scrollbar h-[450px] overflow-y-auto px-2 pb-3">
               <div>
-                <h5 className="mb-5 text-lg font-medium text-gray-800 dark:text-white/90 lg:mb-6">
+                <h5 className="mb-5 text-lg font-medium text-gray-800 lg:mb-6 dark:text-white/90">
                   Social Links
                 </h5>
 
@@ -141,7 +165,7 @@ export default function UserInfoCard() {
                 </div>
               </div>
               <div className="mt-7">
-                <h5 className="mb-5 text-lg font-medium text-gray-800 dark:text-white/90 lg:mb-6">
+                <h5 className="mb-5 text-lg font-medium text-gray-800 lg:mb-6 dark:text-white/90">
                   Personal Information
                 </h5>
 
@@ -170,10 +194,50 @@ export default function UserInfoCard() {
                     <Label>Bio</Label>
                     <Input type="text" defaultValue="Team Manager" />
                   </div>
+
+                  <div className="col-span-2 lg:col-span-1">
+                    <Label>NickName</Label>
+                    <Input type="text" defaultValue="Bonus" />
+                  </div>
+
+                  <div className="col-span-2 lg:col-span-1">
+                    <Label>Gender</Label>
+                    <Input type="text" defaultValue="Female" />
+                  </div>
+
+                  <div className="col-span-2 lg:col-span-1">
+                    <Label>Age & Birthday</Label>
+                    <Input type="text" defaultValue="28 (April 15, 1995)" />
+                  </div>
+
+                  <div className="col-span-2 lg:col-span-1">
+                    <Label>Blood Group</Label>
+                    <Input type="text" defaultValue="O+" />
+                  </div>
+
+                  <div className="col-span-2 lg:col-span-1">
+                    <Label>Personality</Label>
+                    <Input type="text" defaultValue="ENFJ" />
+                  </div>
+
+                  <div className="col-span-2 lg:col-span-1">
+                    <Label>Hobby</Label>
+                    <Input type="text" defaultValue="Reading,Sleep" />
+                  </div>
+
+                  <div className="col-span-2 lg:col-span-1">
+                    <Label>อาหารที่ชอบ</Label>
+                    <Input type="text" defaultValue="ก๋วยเตี๋ยวเรือ" />
+                  </div>
+
+                  <div className="col-span-2 lg:col-span-1">
+                    <Label>อาหารที่ไม่ชอบ</Label>
+                    <Input type="text" defaultValue="หัวหอม" />
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="flex items-center gap-3 px-2 mt-6 lg:justify-end">
+            <div className="mt-6 flex items-center gap-3 px-2 lg:justify-end">
               <Button size="sm" variant="outline" onClick={closeModal}>
                 Close
               </Button>
